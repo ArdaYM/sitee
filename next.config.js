cb11b37625a99c01cae029931f9b3bb964a431bf
ourlex <arda5976@gmail.com>
2025-12-01 22:34:27 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
-
-
+module.exports = nextConfig;

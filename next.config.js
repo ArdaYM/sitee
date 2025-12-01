e5d37a4fb338b355aea366aa28a028197a0f1629
ourlex <arda5976@gmail.com>
2025-12-01 22:37:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: "/sitee", // <-- senin repository adı
+  assetPrefix: "/sitee", // <-- aynı isim
 };
 
 module.exports = nextConfig;

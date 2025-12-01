b67b40e265c4102a2d0b3860bdf05ced450ae6da
ourlex <arda5976@gmail.com>
2025-12-01 22:58:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
             </div>
             <div className="relative h-96 lg:h-[500px] rounded-lg overflow-hidden shadow-2xl">
               <Image
-                src="/sitee/public/logo.png"
+                src="/sitee/images/hero-havalandirma.jpg"
                 alt="ARYUS - Yaşam ve Çalışma Alanları Çözümleri"
                 fill
                 className="object-cover"
@@ -177,6 +177,7 @@ export default function Home() {
     </div>
   )
 }
+
 
 
 

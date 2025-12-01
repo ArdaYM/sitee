0c90ad4e5305c69f7e92f9c7dd0788ea28c1c755
ourlex <arda5976@gmail.com>
2025-12-01 23:09:53 +0300

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,20 +5,16 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'ARYUS',
     short_name: 'ARYUS',
     description: 'Klima ve havalandırma sistemleri temizliği, bakımı ve teknik servis hizmetleri',
-    start_url: '/',
+    start_url: '/sitee/', // basePath eklendi
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#0079e6',
     icons: [
       {
-        src: '/logo.png',
+        src: '/sitee/logo.png', // basePath eklendi
         sizes: 'any',
         type: 'image/png',
       },
     ],
   }
 }
-
-
-
-
